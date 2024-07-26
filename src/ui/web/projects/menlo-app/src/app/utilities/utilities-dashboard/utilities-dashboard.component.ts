f13ab944b5aa8721c3805f48b734a0914d6eb471
Wilco Boshoff <wlboshoff@gmail.com>
2024-07-26 15:31:32 +0200

    UI: adding electricity usage to util dash
    The Utilities Dashboard will now display the Electricity Usage by means
    of the component created.
    
    Additionally this migrates to the use of signals over inputs in Angular.
    Pretty cool, though I hate that props looks like methods.

--- src/ui/web/projects/menlo-app/src/app/utilities/utilities-dashboard/utilities-dashboard.component.ts
+++ src/ui/web/projects/menlo-app/src/app/utilities/utilities-dashboard/utilities-dashboard.component.ts
@@ -1,10 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Signal } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
+import { ElectricityUsageComponent } from '../electricity/electricity-usage/electricity-usage.component';
+import { UtilitiesService } from '@utilities/utilities.service';
+import { map, Observable, takeUntil } from 'rxjs';
+import { ElectricityUsageQueryFactory } from '@utilities/electricity';
+import { ElectricityUsage } from '@utilities/electricity/electricity-usage/electricity-usage.model';
+import { DestroyableComponent } from 'menlo-lib';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
     standalone: true,
-    imports: [CommonModule, RouterLinkWithHref],
+    imports: [AsyncPipe, JsonPipe, RouterLinkWithHref, ElectricityUsageComponent],
     template: `<header class="d-flex flex-nowrap p-0">
             <h1 class="me-auto">Utilities</h1>
             <div class="px-1">
@@ -14,8 +21,36 @@ import { RouterLinkWithHref } from '@angular/router';
                 </a>
             </div>
         </header>
-        <article></article>`,
+        <article class="h-100">
+            <h3>Electricity Usage</h3>
+            <menlo-electricity-usage [electricityUsage]="electricityUsage()" />
+        </article>`,
     styleUrl: './utilities-dashboard.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UtilitiesDashboardComponent {}
+export class UtilitiesDashboardComponent extends DestroyableComponent {
+    public readonly electricityUsage: Signal<ElectricityUsage[]>;
+
+    constructor(private readonly _utilitiesService: UtilitiesService) {
+        super();
+        this.electricityUsage = toSignal(this.setupElectricityUsageObservable(), { initialValue: [] });
+    }
+
+    private setupElectricityUsageObservable(): Observable<ElectricityUsage[]> {
+        const today = new Date();
+        const startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+        const request = ElectricityUsageQueryFactory.create(startDate, today);
+        return this._utilitiesService.getElectricityUsage(request).pipe(
+            map(response =>
+                response.map(
+                    item =>
+                        <ElectricityUsage>{
+                            date: item.date,
+                            units: item.units
+                        }
+                )
+            ),
+            takeUntil(this.destroyed$)
+        );
+    }
+}
