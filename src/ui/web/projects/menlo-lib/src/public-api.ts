c2fc3752a6e7ac7c7d6db33c4156e67112324894
Wilco Boshoff <wlboshoff@gmail.com>
2024-08-10 13:40:23 +0200

    Adding NOOP Component
    Adding a component that does nothing that will primarily be used as a
    component for router testing and other scenarios where a component is
    needed but that doesn't need to do anything.

--- src/ui/web/projects/menlo-lib/src/public-api.ts
+++ src/ui/web/projects/menlo-lib/src/public-api.ts
@@ -5,4 +5,5 @@ export * from './lib/forms/form-buttons/form-buttons.component';
 export * from './lib/layout/root/root.component';
 export * from './lib/nav-item.model';
 export * from './lib/destroyable.component';
+export * from './lib/noop.component';
 export * from './lib/types/date-or-string.type';
