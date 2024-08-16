3767eb046d0ceb98790a2b5aad7a16686bf91872
Wilco Boshoff <wlboshoff@gmail.com>
2024-08-16 05:35:44 +0200

    Updating elec purchase back-end calculation
    The logic for electricity purchases wasn't being correctly calculated. A
    unit test has been added and the logic has been corrected so the current
    day's usage factors in purchases from the previous day when calculating
    the usage.

---
+++ src/lib/Menlo.Lib.UnitTests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Menlo.Common;
+global using Microsoft.Azure.CosmosRepository;
+global using Microsoft.Extensions.Logging.Abstractions;
+global using NSubstitute;
+global using Shouldly;
--- src/lib/Menlo.Lib/Utilities/Handlers/Electricity/ElectricityUsageQuery.cs
+++ src/lib/Menlo.Lib/Utilities/Handlers/Electricity/ElectricityUsageQuery.cs
@@ -4,17 +4,19 @@ using Menlo.Utilities.Extensions;
 using Menlo.Utilities.Models;
 using Microsoft.Azure.CosmosRepository;
 using Microsoft.Extensions.Logging;
+using System.Collections.Immutable;
 
 namespace Menlo.Utilities.Handlers.Electricity;
 
 [DateRangeParsable]
 public partial record ElectricityUsageQuery;
 
-internal class ElectricityUsageQueryHandler(ILogger<ElectricityUsageQueryHandler> logger, IRepository<ElectricityUsage> repo)
+internal class ElectricityUsageQueryHandler(ILogger<ElectricityUsageQueryHandler> logger, IRepository<ElectricityUsage> repoUsages, IRepository<ElectricityPurchase> repoPurchases)
     : IQueryHandler<ElectricityUsageQuery, IEnumerable<ElectricityUsageQueryResponse>>
 {
     private readonly ILogger<ElectricityUsageQueryHandler> _logger = logger;
-    private readonly IRepository<ElectricityUsage> _repo = repo;
+    private readonly IRepository<ElectricityUsage> _repoUsages = repoUsages;
+    private readonly IRepository<ElectricityPurchase> _repoPurchases = repoPurchases;
 
     public async Task<Response<IEnumerable<ElectricityUsageQueryResponse>, MenloError>> HandleAsync(
         ElectricityUsageQuery query,
@@ -22,43 +24,62 @@ internal class ElectricityUsageQueryHandler(ILogger<ElectricityUsageQueryHandler
     {
         _logger.HandlingElectricityUsageQuery(query);
 
-        IEnumerable<ElectricityUsage> usages = await _repo.GetByQueryAsync(query.GetCosmosQuery(), cancellationToken);
+        IEnumerable<ElectricityUsage> usages = await _repoUsages.GetByQueryAsync(query.GetCosmosQuery(), cancellationToken);
+        IEnumerable<ElectricityPurchase> purchases = await _repoPurchases.GetByQueryAsync(query.GetPurchaseCosmosQuery(), cancellationToken);
+
+        usages = usages.OrderByDescending(usage => usage.Date);
+        purchases = purchases.OrderByDescending(purchase => purchase.Date);
+
+        List<ElectricityUsageQueryResponse> data = [];
+        ElectricityUsageQueryResponseBuilder builder;
+        foreach (ElectricityUsage usage in usages)
+        {
+            // Possibly optimize this.
+            // We could use a queue to store the previous usage records and pop them off as we iterate through the usage records.
+            ElectricityUsage? previousUsage = usages.FirstOrDefault(u => u.Date == usage.Date.AddDays(-1));
+            ElectricityPurchase? purchase = purchases.FirstOrDefault(p => p.Date == usage.Date.AddDays(-1));
+
+            builder = new ElectricityUsageQueryResponseBuilder
+            {
+                CurrentUsageRecord = usage,
+                PreviousUsageRecord = previousUsage,
+                PurchaseRecord = purchase
+            };
+
+            ElectricityUsageQueryResponse queryResponse = builder.Build();
+            data.Add(queryResponse);
+        }
 
         return new Response<IEnumerable<ElectricityUsageQueryResponse>, MenloError>()
         {
-            Data = usages.Select(ElectricityUsageQueryResponse.From)
+            Data = data
+            .OrderBy(item => item.Date)
+            .Where(item => item.Date >= query.StartDateTimeOffset && item.Date <= query.EndDateTimeOffset)
+            .ToImmutableList()
         };
     }
 }
 
 public record class ElectricityUsageQueryResponse
 {
-    public DateTimeOffset Date { get; private set; }
-    public decimal Units { get; private set; }
-    public IEnumerable<ApplianceUsageQueryResponse> ApplianceUsages { get; private set; } = [];
+    public required DateTimeOffset Date { get; init; }
+    public required decimal Units { get; init; }
+    public required decimal Usage { get; init; }
 
-    internal static ElectricityUsageQueryResponse From(ElectricityUsage usage)
+    public required IImmutableList<ApplianceUsageQueryResponse> ApplianceUsages { get; init; }
+}
+
+public class ApplianceUsageQueryResponse
+{
+    public int ApplianceId { get; private set; }
+    public decimal HoursOfUse { get; private set; }
+
+    internal static ApplianceUsageQueryResponse From(ApplianceUsage applianceUsage)
     {
-        return new ElectricityUsageQueryResponse
+        return new ApplianceUsageQueryResponse
         {
-            Date = usage.Date,
-            Units = usage.Units,
-            ApplianceUsages = usage.ApplianceUsages?.Select(ApplianceUsageQueryResponse.From) ?? []
+            ApplianceId = applianceUsage.ApplianceId,
+            HoursOfUse = applianceUsage.HoursOfUse
         };
-    }
-
-    public class ApplianceUsageQueryResponse
-    {
-        public int ApplianceId { get; private set; }
-        public decimal HoursOfUse { get; private set; }
-
-        internal static ApplianceUsageQueryResponse From(ApplianceUsage applianceUsage)
-        {
-            return new ApplianceUsageQueryResponse
-            {
-                ApplianceId = applianceUsage.ApplianceId,
-                HoursOfUse = applianceUsage.HoursOfUse
-            };
-        }
     }
 }
