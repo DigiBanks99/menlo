90ce269f9b87a5459850f8ccc68aad7d509d8241
Wilco Boshoff <wlboshoff@gmail.com>
2025-01-07 06:10:00 +0200

    Refactor API scaffolding
    This commit refactors the structure of the Endpoint construction so
    Electricity endpoints are contained in their own file and only
    referenced in the Utilities MapEndpongs. The OpenAPI DateRangeParsable
    mapping has been moved to its own class for reuse by other components.
    
    Finally, auth has been added to the http test endpoints.

---
+++ src/api/Menlo.Api/Common/OpenApiExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Menlo.Common;
+
+public static partial class OpenApiExtensions
+{
+    [System.Text.RegularExpressions.GeneratedRegex(@"[+-]{1}\d2:\d2")]
+    private static partial System.Text.RegularExpressions.Regex TimeZonePatternRegex();
+
+    public static RouteHandlerBuilder WithDateRangeQuery(this RouteHandlerBuilder route)
+    {
+        return route.WithOpenApi(o =>
+        {
+            o.Parameters.Add(new OpenApiParameter
+            {
+                Name = "startDate",
+                AllowEmptyValue = false,
+                Description = "The start date of the usages that must be returned",
+                In = ParameterLocation.Query,
+                Required = true,
+                Schema = new() { Format = "date", Type = "string" }
+            });
+
+            o.Parameters.Add(new OpenApiParameter
+            {
+                Name = "endDate",
+                AllowEmptyValue = true,
+                Description = "The end date of the usages that must be returned",
+                In = ParameterLocation.Query,
+                Required = false,
+                Schema = new() { Format = "date", Type = "string" }
+            });
+
+            o.Parameters.Add(new OpenApiParameter
+            {
+                Name = "timeZone",
+                AllowEmptyValue = true,
+                Description = "The time zone of the client",
+                In = ParameterLocation.Query,
+                Required = false,
+                Schema = new()
+                {
+                    Format = "timestamp", Type = "string", Pattern = TimeZonePatternRegex().ToString()
+                }
+            });
+
+            return o;
+        });
+    }
+}
